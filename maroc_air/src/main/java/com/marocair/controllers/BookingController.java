6f3d9aacc0e53d87c5426a9efb2bcdf8df12d363
Saad Chaay <93975374+saadchaay@users.noreply.github.com>
2022-10-18 17:31:20 +0100

    Update BookingController.java

--- maroc_air/src/main/java/com/marocair/controllers/BookingController.java
+++ maroc_air/src/main/java/com/marocair/controllers/BookingController.java
@@ -5,6 +5,7 @@ import com.marocair.dao.ClientDao;
 import com.marocair.dao.ReservationsDao;
 import com.marocair.dao.RoutesDao;
 import com.marocair.helpers.SendMail;
+import com.marocair.models.Cities;
 import com.marocair.models.Clients;
 import com.marocair.models.Reservations;
 import com.marocair.models.Routes;
@@ -15,6 +16,7 @@ import jakarta.servlet.annotation.*;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Objects;
 
 @WebServlet(name = "BookingController", value = "/reservation")
@@ -58,27 +60,37 @@ public class BookingController extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         long routeId = Long.parseLong(request.getParameter("routeId"));
-        Routes route = null;
+        Routes route;
+        ArrayList<Cities> cities;
         try {
+            cities = (ArrayList<Cities>) citiesDao.getAll();
             route = routesDao.get(routeId).isPresent() ? routesDao.get(routeId).get() : null;
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
         assert route != null;
-        System.out.println(route.getStart_city());
         client.setFullName(request.getParameter("first_name")+" "+request.getParameter("last_name"));
         client.setEmail(request.getParameter("email"));
         client.setPhone(request.getParameter("phone"));
         if(clientDao.save(client)){
             try {
+                String startCity = null;
+                String arrivalCity = null;
                 int idClient = clientDao.getClientId();
                 int random_ref = (int)Math.floor(Math.random()*(999999-99999+1)+99999);
                 res.setClient_id(idClient);
                 res.setReference(String.valueOf(random_ref));
                 res.setDepartDate(LocalDate.now().plusDays(10).toString());
                 if(resDao.save(res)){
-                    String confirmMsg = "Reference number: "+res.getReference()+"\nYour Trip: \n\tStart city >> "+route.getStart_city()+"\t\t Start time: " + route.getStart_time() +
-                            "\n\tArrival city >> "+route.getArrival_city()+ "\n\t\t\t Price:  "+ route.getPrice();
+                    for (Cities city: cities){
+                        if(city.getId() == route.getStart_city())
+                            startCity = city.getName();
+                        else if (city.getId() == route.getArrival_city()) {
+                            arrivalCity = city.getName();
+                        }
+                    }
+                    String confirmMsg = "Reference number: "+res.getReference()+"\nYour Trip Date:  " + res.getDepartDate() + " \n\tStart city -    "+ startCity +"\t\t,    Start time:     " + route.getStart_time() +
+                            "\n\tArrival city -      "+ arrivalCity + "\n\t\t\t Duration:    "+ Integer.parseInt(String.valueOf(route.getDuration()/60)) +" H "+ (route.getDuration()%60) + " Minutes";
                     SendMail.sendMessage(client.getEmail(),"Confirmation reservation", confirmMsg);
                     request.setAttribute("refBooking", res.getReference());
                     request.setAttribute("successMsg", "Reservation has been booked successfully");
